f45a8e27a4352e44ec5954fdf7f5f15cb03727b6
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-03-25 22:16:15 +0600

    refactor code

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
+
+// select items
 const navToggle = document.querySelector('.nav-toggle');
 const menuArea = document.querySelector('.menu-area');
-// const sticky = document.querySelector('#sticky');
 
 
-
+// mobile menu open/close
 navToggle.addEventListener('click', () => {
 
     menuArea.classList.toggle('toggle');
 })
 
-
-
-
-// ********** fixed navbar ************
-
-
-// ********** smooth scroll ************
-// select links
-
-
-
-// console.log(window.scrollTo);
-
+//Desktop menu fixed when scroll
 window.addEventListener('scroll', () => {
 
-    // const height = window.pageYOffset;
-    // console.log(height, "pageYOffset");
-    
+    if (window.pageYOffset >= 90) {
+
+        navbar.classList.add("sticky");
+    } else {
+
+        navbar.classList.remove("sticky");
+    }
 })
+
