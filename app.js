f0a90ea47065ed40aabf0f238c3f31430e437b7c
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-03-25 20:22:13 +0600

    finished

--- app.js
+++ app.js
@@ -4,10 +4,37 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const menuArea = document.querySelector('.menu-area');
+// const sticky = document.querySelector('#sticky');
+
+
+
+navToggle.addEventListener('click', () => {
+
+    menuArea.classList.toggle('toggle');
+})
+
+
+
 
 // ********** fixed navbar ************
 
+
 // ********** smooth scroll ************
 // select links
+
+
+
+// console.log(window.scrollTo);
+
+window.addEventListener('scroll', () => {
+
+    // const height = window.pageYOffset;
+    // console.log(height, "pageYOffset");
+    
+})
